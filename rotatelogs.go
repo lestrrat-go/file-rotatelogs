6a4e0f9003cbef335d7f782833d23d1c4ad629ad
Daisuke Maki <lestrrat+github@gmail.com>
2014-04-06 21:23:15 +0900

    some linting

--- rotatelogs.go
+++ rotatelogs.go
@@ -7,7 +7,6 @@ Port of File-RotateLogs from Perl (https://metacpan.org/release/File-RotateLogs)
 package rotatelogs
 
 import(
-  "errors"
   "fmt"
   "os"
   "path/filepath"
@@ -49,33 +48,33 @@ func NewRotateLogs(logfile string) (*RotateLogs) {
   }
 }
 
-func (self *RotateLogs) GenFilename() (string, error) {
+func (rl *RotateLogs) GenFilename() (string, error) {
   now := CurrentTime()
-  diff := time.Duration(now.Add(self.Offset).UnixNano()) % self.RotationTime
+  diff := time.Duration(now.Add(rl.Offset).UnixNano()) % rl.RotationTime
   t   := now.Add(time.Duration(-1 * diff))
-  str, err :=  strftime.Format(self.LogFile, t)
+  str, err :=  strftime.Format(rl.LogFile, t)
   if err != nil {
     return "", err
   }
   return str, err
 }
 
-func (self *RotateLogs) Write(p []byte) (n int, err error) {
+func (rl *RotateLogs) Write(p []byte) (n int, err error) {
   // Guard against concurrent writes
-  self.sem <-true
-  defer func() { <-self.sem }()
+  rl.sem <-true
+  defer func() { <-rl.sem }()
 
   // This filename contains the name of the "NEW" filename
-  // to log to, which may be newer than self.currentFilename
+  // to log to, which may be newer than rl.currentFilename
 
-  filename, err := self.GenFilename()
+  filename, err := rl.GenFilename()
   if err != nil {
     return 0, err
   }
 
   var out *os.File
-  if filename == self.curFn { // Match!
-    out = self.outFh // use old one
+  if filename == rl.curFn { // Match!
+    out = rl.outFh // use old one
   }
 
   isNew := false
@@ -84,8 +83,8 @@ func (self *RotateLogs) Write(p []byte) (n int, err error) {
 
     _, err := os.Stat(filename)
     if err == nil {
-      if self.LinkName != "" {
-        _, err = os.Lstat(self.LinkName)
+      if rl.LinkName != "" {
+        _, err = os.Lstat(rl.LinkName)
         if err == nil {
           isNew = false
         }
@@ -94,51 +93,49 @@ func (self *RotateLogs) Write(p []byte) (n int, err error) {
 
     fh, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
     if err != nil {
-      return 0, errors.New(
-        fmt.Sprintf("Failed to open file %s: %s", self.LogFile, err),
-      )
+      return 0, fmt.Errorf("error: Failed to open file %s: %s", rl.LogFile, err)
     }
 
     out = fh
     if isNew {
-      self.Rotate(filename)
+      rl.Rotate(filename)
     }
   }
 
   n, err = out.Write(p)
 
-  if self.outFh == nil {
-    self.outFh = out
+  if rl.outFh == nil {
+    rl.outFh = out
   } else if isNew {
-    self.outFh.Close()
-    self.outFh = out
+    rl.outFh.Close()
+    rl.outFh = out
   }
-  self.curFn = filename
+  rl.curFn = filename
 
   return n, err
 }
 
-func (self *RotateLogs) CurrentFileName() string {
-  return self.curFn
+func (rl *RotateLogs) CurrentFileName() string {
+  return rl.curFn
 }
 
 var patternConversionRegexps = []*regexp.Regexp {
   regexp.MustCompile(`%[%+A-Za-z]`),
   regexp.MustCompile(`\*+`),
 }
-func (self *RotateLogs) LogFilePattern() string {
-  if self.logfilePattern == "" {
-    lf := self.LogFile
+func (rl *RotateLogs) LogFilePattern() string {
+  if rl.logfilePattern == "" {
+    lf := rl.LogFile
 
     for _, re := range patternConversionRegexps {
       lf = re.ReplaceAllString(lf, "*")
     }
-    self.logfilePattern = lf
+    rl.logfilePattern = lf
   }
-  return self.logfilePattern
+  return rl.logfilePattern
 }
 
-func (self *RotateLogs) Rotate(filename string) (error) {
+func (rl *RotateLogs) Rotate(filename string) (error) {
   lockfn := fmt.Sprintf("%s_lock", filename)
   fh, err := os.OpenFile(lockfn, os.O_CREATE|os.O_EXCL, 0644)
   if err != nil {
@@ -157,31 +154,31 @@ func (self *RotateLogs) Rotate(filename string) (error) {
     }
   }()
 
-  if self.LinkName != "" {
+  if rl.LinkName != "" {
     tmpLinkName := fmt.Sprintf("%s_symlink", filename)
     err = os.Symlink(filename, tmpLinkName)
     if err != nil {
       return err
     }
 
-    err = os.Rename(tmpLinkName, self.LinkName)
+    err = os.Rename(tmpLinkName, rl.LinkName)
     if err != nil {
       return err
     }
   }
 
-  if self.MaxAge <= 0 {
+  if rl.MaxAge <= 0 {
     return nil
   }
 
-  pattern := self.LogFilePattern()
+  pattern := rl.LogFilePattern()
   matches, err := filepath.Glob(pattern)
   if err != nil {
     return err
   }
 
-  cutoff := CurrentTime().Add(-1 * self.MaxAge)
-  var to_unlink []string
+  cutoff := CurrentTime().Add(-1 * rl.MaxAge)
+  var toUnlink []string
   for _, path := range matches {
     // Ignore lock files
     if strings.HasSuffix(path, "_lock") || strings.HasSuffix(path, "_symlink"){
@@ -196,10 +193,10 @@ func (self *RotateLogs) Rotate(filename string) (error) {
     if fi.ModTime().After(cutoff) {
       continue
     }
-    to_unlink = append(to_unlink, path)
+    toUnlink = append(toUnlink, path)
   }
 
-  if len(to_unlink) <= 0 {
+  if len(toUnlink) <= 0 {
     return nil
   }
 
@@ -207,7 +204,7 @@ func (self *RotateLogs) Rotate(filename string) (error) {
   go func () {
     defer guard()
     // unlink files on a separate goroutine
-    for _, path := range to_unlink {
+    for _, path := range toUnlink {
       os.Remove(path)
     }
   }()
@@ -215,10 +212,10 @@ func (self *RotateLogs) Rotate(filename string) (error) {
   return nil
 }
 
-func (self *RotateLogs) Close() (error) {
-  if self.outFh != nil {
-    self.outFh.Close()
-    self.outFh = nil
+func (rl *RotateLogs) Close() (error) {
+  if rl.outFh != nil {
+    rl.outFh.Close()
+    rl.outFh = nil
   }
   return nil
 }
