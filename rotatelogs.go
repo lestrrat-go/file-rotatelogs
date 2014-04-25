67043cf91e24f0619092f640678477596ed612bf
Daisuke Maki <lestrrat+github@gmail.com>
2014-04-25 14:52:19 +0900

    I'm a born again go-hard-tabber

--- rotatelogs.go
+++ rotatelogs.go
@@ -6,27 +6,27 @@ Port of File-RotateLogs from Perl (https://metacpan.org/release/File-RotateLogs)
 
 package rotatelogs
 
-import(
-  "fmt"
-  "os"
-  "path/filepath"
-  "regexp"
-  "strings"
-  "time"
-  "bitbucket.org/tebeka/strftime"
+import (
+	"bitbucket.org/tebeka/strftime"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"time"
 )
 
 type RotateLogs struct {
-  LogFile string
-  LinkName  string
-  RotationTime  time.Duration
-  MaxAge        time.Duration
-  Offset        time.Duration
+	LogFile      string
+	LinkName     string
+	RotationTime time.Duration
+	MaxAge       time.Duration
+	Offset       time.Duration
 
-  curFn string
-  outFh *os.File
-  logfilePattern string
-  sem chan bool
+	curFn          string
+	outFh          *os.File
+	logfilePattern string
+	sem            chan bool
 }
 
 /* CurrentTime is only used for testing. Normally it's the time.Now()
@@ -34,188 +34,189 @@ type RotateLogs struct {
  */
 var CurrentTime = time.Now
 
-func NewRotateLogs(logfile string) (*RotateLogs) {
-  return &RotateLogs {
-    logfile,
-    "",
-    86400 * time.Second,
-    0,
-    0,
-    "",
-    nil,
-    "",
-    make(chan bool, 1),
-  }
+func NewRotateLogs(logfile string) *RotateLogs {
+	return &RotateLogs{
+		logfile,
+		"",
+		86400 * time.Second,
+		0,
+		0,
+		"",
+		nil,
+		"",
+		make(chan bool, 1),
+	}
 }
 
 func (rl *RotateLogs) GenFilename() (string, error) {
-  now := CurrentTime()
-  diff := time.Duration(now.Add(rl.Offset).UnixNano()) % rl.RotationTime
-  t   := now.Add(time.Duration(-1 * diff))
-  str, err :=  strftime.Format(rl.LogFile, t)
-  if err != nil {
-    return "", err
-  }
-  return str, err
+	now := CurrentTime()
+	diff := time.Duration(now.Add(rl.Offset).UnixNano()) % rl.RotationTime
+	t := now.Add(time.Duration(-1 * diff))
+	str, err := strftime.Format(rl.LogFile, t)
+	if err != nil {
+		return "", err
+	}
+	return str, err
 }
 
 func (rl *RotateLogs) Write(p []byte) (n int, err error) {
-  // Guard against concurrent writes
-  rl.sem <-true
-  defer func() { <-rl.sem }()
+	// Guard against concurrent writes
+	rl.sem <- true
+	defer func() { <-rl.sem }()
 
-  // This filename contains the name of the "NEW" filename
-  // to log to, which may be newer than rl.currentFilename
+	// This filename contains the name of the "NEW" filename
+	// to log to, which may be newer than rl.currentFilename
 
-  filename, err := rl.GenFilename()
-  if err != nil {
-    return 0, err
-  }
+	filename, err := rl.GenFilename()
+	if err != nil {
+		return 0, err
+	}
 
-  var out *os.File
-  if filename == rl.curFn { // Match!
-    out = rl.outFh // use old one
-  }
+	var out *os.File
+	if filename == rl.curFn { // Match!
+		out = rl.outFh // use old one
+	}
 
-  isNew := false
-  if out == nil {
-    isNew = true
+	isNew := false
+	if out == nil {
+		isNew = true
 
-    _, err := os.Stat(filename)
-    if err == nil {
-      if rl.LinkName != "" {
-        _, err = os.Lstat(rl.LinkName)
-        if err == nil {
-          isNew = false
-        }
-      }
-    }
+		_, err := os.Stat(filename)
+		if err == nil {
+			if rl.LinkName != "" {
+				_, err = os.Lstat(rl.LinkName)
+				if err == nil {
+					isNew = false
+				}
+			}
+		}
 
-    fh, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
-    if err != nil {
-      return 0, fmt.Errorf("error: Failed to open file %s: %s", rl.LogFile, err)
-    }
+		fh, err := os.OpenFile(filename, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+		if err != nil {
+			return 0, fmt.Errorf("error: Failed to open file %s: %s", rl.LogFile, err)
+		}
 
-    out = fh
-    if isNew {
-      rl.Rotate(filename)
-    }
-  }
+		out = fh
+		if isNew {
+			rl.Rotate(filename)
+		}
+	}
 
-  n, err = out.Write(p)
+	n, err = out.Write(p)
 
-  if rl.outFh == nil {
-    rl.outFh = out
-  } else if isNew {
-    rl.outFh.Close()
-    rl.outFh = out
-  }
-  rl.curFn = filename
+	if rl.outFh == nil {
+		rl.outFh = out
+	} else if isNew {
+		rl.outFh.Close()
+		rl.outFh = out
+	}
+	rl.curFn = filename
 
-  return n, err
+	return n, err
 }
 
 func (rl *RotateLogs) CurrentFileName() string {
-  return rl.curFn
+	return rl.curFn
 }
 
-var patternConversionRegexps = []*regexp.Regexp {
-  regexp.MustCompile(`%[%+A-Za-z]`),
-  regexp.MustCompile(`\*+`),
+var patternConversionRegexps = []*regexp.Regexp{
+	regexp.MustCompile(`%[%+A-Za-z]`),
+	regexp.MustCompile(`\*+`),
 }
+
 func (rl *RotateLogs) LogFilePattern() string {
-  if rl.logfilePattern == "" {
-    lf := rl.LogFile
+	if rl.logfilePattern == "" {
+		lf := rl.LogFile
 
-    for _, re := range patternConversionRegexps {
-      lf = re.ReplaceAllString(lf, "*")
-    }
-    rl.logfilePattern = lf
-  }
-  return rl.logfilePattern
+		for _, re := range patternConversionRegexps {
+			lf = re.ReplaceAllString(lf, "*")
+		}
+		rl.logfilePattern = lf
+	}
+	return rl.logfilePattern
 }
 
-func (rl *RotateLogs) Rotate(filename string) (error) {
-  lockfn := fmt.Sprintf("%s_lock", filename)
-  fh, err := os.OpenFile(lockfn, os.O_CREATE|os.O_EXCL, 0644)
-  if err != nil {
-    // Can't lock, just return
-    return err
-  }
+func (rl *RotateLogs) Rotate(filename string) error {
+	lockfn := fmt.Sprintf("%s_lock", filename)
+	fh, err := os.OpenFile(lockfn, os.O_CREATE|os.O_EXCL, 0644)
+	if err != nil {
+		// Can't lock, just return
+		return err
+	}
 
-  goneToBackground := false
-  guard := func() {
-    fh.Close()
-    os.Remove(lockfn)
-  }
-  defer func() {
-    if ! goneToBackground {
-      guard()
-    }
-  }()
+	goneToBackground := false
+	guard := func() {
+		fh.Close()
+		os.Remove(lockfn)
+	}
+	defer func() {
+		if !goneToBackground {
+			guard()
+		}
+	}()
 
-  if rl.LinkName != "" {
-    tmpLinkName := fmt.Sprintf("%s_symlink", filename)
-    err = os.Symlink(filename, tmpLinkName)
-    if err != nil {
-      return err
-    }
+	if rl.LinkName != "" {
+		tmpLinkName := fmt.Sprintf("%s_symlink", filename)
+		err = os.Symlink(filename, tmpLinkName)
+		if err != nil {
+			return err
+		}
 
-    err = os.Rename(tmpLinkName, rl.LinkName)
-    if err != nil {
-      return err
-    }
-  }
+		err = os.Rename(tmpLinkName, rl.LinkName)
+		if err != nil {
+			return err
+		}
+	}
 
-  if rl.MaxAge <= 0 {
-    return nil
-  }
+	if rl.MaxAge <= 0 {
+		return nil
+	}
 
-  pattern := rl.LogFilePattern()
-  matches, err := filepath.Glob(pattern)
-  if err != nil {
-    return err
-  }
+	pattern := rl.LogFilePattern()
+	matches, err := filepath.Glob(pattern)
+	if err != nil {
+		return err
+	}
 
-  cutoff := CurrentTime().Add(-1 * rl.MaxAge)
-  var toUnlink []string
-  for _, path := range matches {
-    // Ignore lock files
-    if strings.HasSuffix(path, "_lock") || strings.HasSuffix(path, "_symlink"){
-      continue
-    }
+	cutoff := CurrentTime().Add(-1 * rl.MaxAge)
+	var toUnlink []string
+	for _, path := range matches {
+		// Ignore lock files
+		if strings.HasSuffix(path, "_lock") || strings.HasSuffix(path, "_symlink") {
+			continue
+		}
 
-    fi, err := os.Stat(path)
-    if err != nil {
-      continue
-    }
+		fi, err := os.Stat(path)
+		if err != nil {
+			continue
+		}
 
-    if fi.ModTime().After(cutoff) {
-      continue
-    }
-    toUnlink = append(toUnlink, path)
-  }
+		if fi.ModTime().After(cutoff) {
+			continue
+		}
+		toUnlink = append(toUnlink, path)
+	}
 
-  if len(toUnlink) <= 0 {
-    return nil
-  }
+	if len(toUnlink) <= 0 {
+		return nil
+	}
 
-  goneToBackground = true
-  go func () {
-    defer guard()
-    // unlink files on a separate goroutine
-    for _, path := range toUnlink {
-      os.Remove(path)
-    }
-  }()
+	goneToBackground = true
+	go func() {
+		defer guard()
+		// unlink files on a separate goroutine
+		for _, path := range toUnlink {
+			os.Remove(path)
+		}
+	}()
 
-  return nil
+	return nil
 }
 
-func (rl *RotateLogs) Close() (error) {
-  if rl.outFh != nil {
-    rl.outFh.Close()
-    rl.outFh = nil
-  }
-  return nil
+func (rl *RotateLogs) Close() error {
+	if rl.outFh != nil {
+		rl.outFh.Close()
+		rl.outFh = nil
+	}
+	return nil
 }
--- rotatelogs_test.go
+++ rotatelogs_test.go
@@ -1,179 +1,179 @@
 package rotatelogs
 
 import (
-  "fmt"
-  "io/ioutil"
-  "log"
-  "os"
-  "path/filepath"
-  "strings"
-  "testing"
-  "time"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+	"time"
 )
 
 func TestGenFilename(t *testing.T) {
-  // Mock time
-  ts := []time.Time {
-    time.Time {},
-    (time.Time {}).Add(24 * time.Hour),
-  }
+	// Mock time
+	ts := []time.Time{
+		time.Time{},
+		(time.Time{}).Add(24 * time.Hour),
+	}
 
-  var old = CurrentTime
-  defer func() { CurrentTime = old }()
-  for _, xt := range ts {
-    CurrentTime = func() (time.Time) { return xt }
-    rl := NewRotateLogs("/path/to/%Y/%m/%d")
-    defer rl.Close()
+	var old = CurrentTime
+	defer func() { CurrentTime = old }()
+	for _, xt := range ts {
+		CurrentTime = func() time.Time { return xt }
+		rl := NewRotateLogs("/path/to/%Y/%m/%d")
+		defer rl.Close()
 
-    fn, err := rl.GenFilename()
-    if err != nil {
-      t.Errorf("Failed to generate filename: %s", err)
-    }
+		fn, err := rl.GenFilename()
+		if err != nil {
+			t.Errorf("Failed to generate filename: %s", err)
+		}
 
-    expected := fmt.Sprintf("/path/to/%04d/%02d/%02d",
-      xt.Year(),
-      xt.Month(),
-      xt.Day(),
-    )
+		expected := fmt.Sprintf("/path/to/%04d/%02d/%02d",
+			xt.Year(),
+			xt.Month(),
+			xt.Day(),
+		)
 
-    if fn != expected {
-      t.Errorf("Failed to match fn (%s)", fn)
-    }
-    t.Logf("fn = %s", fn)
-  }
+		if fn != expected {
+			t.Errorf("Failed to match fn (%s)", fn)
+		}
+		t.Logf("fn = %s", fn)
+	}
 }
 
-func TestLogFilePattern (t *testing.T) {
-  rl := NewRotateLogs("/path/to/%Y/%m/%d")
-  defer rl.Close()
-  pattern := rl.LogFilePattern()
-  if pattern != "/path/to/*/*/*" {
-    t.Errorf("Failed to match pattern (%s)", pattern)
-  }
+func TestLogFilePattern(t *testing.T) {
+	rl := NewRotateLogs("/path/to/%Y/%m/%d")
+	defer rl.Close()
+	pattern := rl.LogFilePattern()
+	if pattern != "/path/to/*/*/*" {
+		t.Errorf("Failed to match pattern (%s)", pattern)
+	}
 }
 
-func TestLogRotate (t *testing.T) {
-  dir, err := ioutil.TempDir("", "file-rotatelogs-test")
-  if err != nil {
-    t.Errorf("Failed to create temporary directory: %s", err)
-  }
-  defer os.RemoveAll(dir)
+func TestLogRotate(t *testing.T) {
+	dir, err := ioutil.TempDir("", "file-rotatelogs-test")
+	if err != nil {
+		t.Errorf("Failed to create temporary directory: %s", err)
+	}
+	defer os.RemoveAll(dir)
 
-  // Change current time, so we can safely purge old logs
-  old := CurrentTime
-  dummyTime := time.Now().Add(-7 * 86400 * time.Second)
-  dummyTime  = dummyTime.Add(time.Duration(-1 * dummyTime.Nanosecond()))
-  defer func() { CurrentTime = old }()
-  CurrentTime = func() (time.Time) { return dummyTime }
+	// Change current time, so we can safely purge old logs
+	old := CurrentTime
+	dummyTime := time.Now().Add(-7 * 86400 * time.Second)
+	dummyTime = dummyTime.Add(time.Duration(-1 * dummyTime.Nanosecond()))
+	defer func() { CurrentTime = old }()
+	CurrentTime = func() time.Time { return dummyTime }
 
-  rl := NewRotateLogs(filepath.Join(dir, "log%Y%m%d%H%M%S"))
-  defer rl.Close()
+	rl := NewRotateLogs(filepath.Join(dir, "log%Y%m%d%H%M%S"))
+	defer rl.Close()
 
-  rl.MaxAge = 86400 * time.Second
-  rl.LinkName = filepath.Join(dir, "log")
+	rl.MaxAge = 86400 * time.Second
+	rl.LinkName = filepath.Join(dir, "log")
 
-  str := "Hello, World"
-  n, err := rl.Write([]byte(str))
-  if n != len(str) {
-    t.Errorf("Could not write %d bytes (wrote %d bytes)", len(str), n)
-  }
+	str := "Hello, World"
+	n, err := rl.Write([]byte(str))
+	if n != len(str) {
+		t.Errorf("Could not write %d bytes (wrote %d bytes)", len(str), n)
+	}
 
-  if err != nil {
-    t.Errorf("Failed to Write() to log: %s", err)
-  }
+	if err != nil {
+		t.Errorf("Failed to Write() to log: %s", err)
+	}
 
-  fn := rl.CurrentFileName()
-  if fn == "" {
-    t.Errorf("Could not get filename %s", fn)
-  }
+	fn := rl.CurrentFileName()
+	if fn == "" {
+		t.Errorf("Could not get filename %s", fn)
+	}
 
-  content, err := ioutil.ReadFile(fn)
-  if err != nil {
-    t.Errorf("Failed to read file %s: %s", fn, err)
-  }
+	content, err := ioutil.ReadFile(fn)
+	if err != nil {
+		t.Errorf("Failed to read file %s: %s", fn, err)
+	}
 
-  if string(content) != str {
-    t.Errorf(`File content does not match (was "%s")`,  content)
-  }
+	if string(content) != str {
+		t.Errorf(`File content does not match (was "%s")`, content)
+	}
 
-  err = os.Chtimes(fn, dummyTime, dummyTime)
-  if err != nil {
-    t.Errorf("Failed to change access/modification times for %s: %s", fn, err)
-  }
+	err = os.Chtimes(fn, dummyTime, dummyTime)
+	if err != nil {
+		t.Errorf("Failed to change access/modification times for %s: %s", fn, err)
+	}
 
-  fi, err := os.Stat(fn)
-  if err != nil {
-    t.Errorf("Failed to stat %s: %s", fn, err)
-  }
+	fi, err := os.Stat(fn)
+	if err != nil {
+		t.Errorf("Failed to stat %s: %s", fn, err)
+	}
 
-  if ! fi.ModTime().Equal(dummyTime) {
-    t.Errorf("Failed to chtime for %s (expected %s, got %s)", fn, fi.ModTime(), dummyTime)
-  }
+	if !fi.ModTime().Equal(dummyTime) {
+		t.Errorf("Failed to chtime for %s (expected %s, got %s)", fn, fi.ModTime(), dummyTime)
+	}
 
-  CurrentTime = old
+	CurrentTime = old
 
-  // This next Write() should trigger Rotate()
-  rl.Write([]byte(str))
-  newfn := rl.CurrentFileName()
-  if newfn == fn {
-    t.Errorf(`New file name and old file name should not match ("%s" != "%s")`, fn, newfn)
-  }
+	// This next Write() should trigger Rotate()
+	rl.Write([]byte(str))
+	newfn := rl.CurrentFileName()
+	if newfn == fn {
+		t.Errorf(`New file name and old file name should not match ("%s" != "%s")`, fn, newfn)
+	}
 
-  content, err = ioutil.ReadFile(newfn)
-  if err != nil {
-    t.Errorf("Failed to read file %s: %s", newfn, err)
-  }
+	content, err = ioutil.ReadFile(newfn)
+	if err != nil {
+		t.Errorf("Failed to read file %s: %s", newfn, err)
+	}
 
-  if string(content) != str {
-    t.Errorf(`File content does not match (was "%s")`,  content)
-  }
+	if string(content) != str {
+		t.Errorf(`File content does not match (was "%s")`, content)
+	}
 
-  time.Sleep(1 * time.Second)
+	time.Sleep(1 * time.Second)
 
-  // fn was declared above, before mocking CurrentTime
-  // Old files should have been unlinked
-  _, err = os.Stat(fn)
-  if err == nil {
-    t.Errorf("Stat succeeded (should have failed) %s: %s", fn, err)
-  }
+	// fn was declared above, before mocking CurrentTime
+	// Old files should have been unlinked
+	_, err = os.Stat(fn)
+	if err == nil {
+		t.Errorf("Stat succeeded (should have failed) %s: %s", fn, err)
+	}
 
-  linkDest, err := os.Readlink(rl.LinkName)
-  if err != nil {
-    t.Errorf("Failed to readlink %s: %s", rl.LinkName, err)
-  }
+	linkDest, err := os.Readlink(rl.LinkName)
+	if err != nil {
+		t.Errorf("Failed to readlink %s: %s", rl.LinkName, err)
+	}
 
-  if linkDest != newfn {
-    t.Errorf(`Symlink destination does not match expected filename ("%s" != "%s")`, newfn, linkDest)
-  }
+	if linkDest != newfn {
+		t.Errorf(`Symlink destination does not match expected filename ("%s" != "%s")`, newfn, linkDest)
+	}
 }
 
-func TestLogSetOutput (t *testing.T) {
-  dir, err := ioutil.TempDir("", "file-rotatelogs-test")
-  if err != nil {
-    t.Errorf("Failed to create temporary directory: %s", err)
-  }
-  defer os.RemoveAll(dir)
+func TestLogSetOutput(t *testing.T) {
+	dir, err := ioutil.TempDir("", "file-rotatelogs-test")
+	if err != nil {
+		t.Errorf("Failed to create temporary directory: %s", err)
+	}
+	defer os.RemoveAll(dir)
 
-  rl := NewRotateLogs(filepath.Join(dir, "log%Y%m%d%H%M%S"))
-  defer rl.Close()
+	rl := NewRotateLogs(filepath.Join(dir, "log%Y%m%d%H%M%S"))
+	defer rl.Close()
 
-  log.SetOutput(rl)
-  defer log.SetOutput(os.Stderr)
+	log.SetOutput(rl)
+	defer log.SetOutput(os.Stderr)
 
-  str := "Hello, World"
-  log.Print(str)
+	str := "Hello, World"
+	log.Print(str)
 
-  fn := rl.CurrentFileName()
-  if fn == "" {
-    t.Errorf("Could not get filename %s", fn)
-  }
+	fn := rl.CurrentFileName()
+	if fn == "" {
+		t.Errorf("Could not get filename %s", fn)
+	}
 
-  content, err := ioutil.ReadFile(fn)
-  if err != nil {
-    t.Errorf("Failed to read file %s: %s", fn, err)
-  }
+	content, err := ioutil.ReadFile(fn)
+	if err != nil {
+		t.Errorf("Failed to read file %s: %s", fn, err)
+	}
 
-  if ! strings.Contains(string(content), str) {
-    t.Errorf(`File content does not contain "%s" (was "%s")`, str, content)
-  }
+	if !strings.Contains(string(content), str) {
+		t.Errorf(`File content does not contain "%s" (was "%s")`, str, content)
+	}
 }
